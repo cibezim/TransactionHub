6f938269b75d7434477913adda1ecf4eff224338
cibezim <git@github.com:cibezim/git_test.git>
2023-11-16 19:13:20 -0500

    Initial Commit

---
+++ transacthub/src/main/java/com/example/transacthub/TransactHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.transacthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactHubApplication.class, args);
+	}
+
+}
